c07078da2d99afc08f5f9217a5065ab00706c1e2
Carl Ian Voller <27472988+carlvoller@users.noreply.github.com>
2023-04-14 15:20:38 +0800

    Update docker.tf

--- docker.tf
+++ docker.tf
@@ -2,29 +2,29 @@
 # build the healthcheck base
 resource "docker_image" "healthcheck_base" {
   name = "healthcheck-base-image"
-  platform = "linux/amd64"
   build {
     context      = local.healthcheck_image_path
     tag = ["healthcheck_base:latest"]
+    platform = "linux/amd64"
   }
 }
 
 # build the base challenge image
 resource "docker_image" "challenge_base" {
   name = "challenge-base-image"
-  platform = "linux/amd64"
   build {
     context = var.challenge_path
     tag  = [local.base_image_name]
+    platform = "linux/amd64"
   }
 }
 
 # generate a chal / jail base image that will be cached and reused between the modules
 resource "docker_image" "nsjail" {
   name = "nsjail-image"
-  platform = "linux/amd64"
   build {
     context      = local.nsjail_image_path
+    platform = "linux/amd64"
     build_arg = {
       NSJAIL_RELEASE = "3.1"
     }
@@ -36,9 +36,9 @@ resource "docker_image" "nsjail" {
 resource "docker_image" "chal" {
   depends_on = [docker_image.nsjail, docker_image.challenge_base]
   name       = "challenge-jailed-image"
-  platform = "linux/amd64"
   build {
     context      = local.jail_image_path
+    platform = "linux/amd64"
     build_arg = {
       CHALLENGE_IMAGE = local.base_image_name
       MEM_LIMIT       = var.memory_limit * 1024 * 1024 // config is in MB, cgroups expects bytes...
@@ -54,10 +54,10 @@ resource "docker_image" "healthcheck" {
   count = var.healthcheck ? 1 : 0
   depends_on = [docker_image.healthcheck_base]
   name = "healthcheck-image"
-  platform = "linux/amd64"
   build {
     context      = var.challenge_path
     dockerfile = "${local.healthcheck_image_path}/Dockerfile_deploy"
+    platform = "linux/amd64"
     build_arg = {
       ADDITIONAL_REQUIREMENTS = var.healthcheck_additional_requirements
     }
